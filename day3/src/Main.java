8a2d0fdc42c8c04129979dce5f88983b218dbb38
JoGyoungJun <yoop80075@gmail.com>
2024-09-25 16:54:06 +0900

    project

--- day3/src/Main.java
+++ day3/src/Main.java
@@ -20,35 +20,45 @@ public class Main{
         list.add(5);
         list.add(4);
         list.add(9);
-        list.add(3);
-        int target = 9;
+        int target = 14;
 //      안에 있는 수 2개 더해서 target 이 되어야함
 //      1 =< list 안에 있는 수 =< 10
 //       answer는 순서 x
         int[] answer = new int[2];
-        Integer[] tmp = new Integer[11];
-        for(int i=0;i<list.size();i++){
-            int num = list.get(i);
-            if(tmp[num] ==null) tmp[num] = i;
-        }
-        System.out.println(Arrays.toString(tmp));
-        for(int i=1;i<tmp.length;i++){
-            if(tmp[i-1] == null)continue;
-            if(target-i < tmp.length && tmp[target-i] != null){
-                int a = tmp[i];
-                int b = tmp[target-i];
-                if(a<b){
-                    answer[0] = i;
-                    answer[1] = target-i;
-                    break;
-                }
-                else {
-                    answer[0] = target-i;
-                    answer[1] = i;
+        Set<Integer> set = new HashSet<>();
+        for (int num : list) {
+            set.add(num);
+            if (set.contains(num)) {
+                if (set.contains(target - num)) {
+                    answer[0] = target - num;
+                    answer[1] = num;
                     break;
                 }
             }
         }
+//        Integer[] tmp = new Integer[11];
+//        for(int i=0;i<list.size();i++){
+//            int num = list.get(i);
+//            if(tmp[num] ==null) tmp[num] = i;
+//        }
+//        System.out.println(Arrays.toString(tmp));
+//        for(int i=1;i<tmp.length;i++){
+//            if(tmp[i-1] == null)continue;
+//            if(target-i < tmp.length && tmp[target-i] != null){
+//                int a = tmp[i];
+//                int b = tmp[target-i];
+//                if(a<b){
+//                    answer[0] = i;
+//                    answer[1] = target-i;
+//                    break;
+//                }
+//                else {
+//                    answer[0] = target-i;
+//                    answer[1] = i;
+//                    break;
+//                }
+//            }
+//        }
         System.out.println(Arrays.toString(answer));
     }
 }
